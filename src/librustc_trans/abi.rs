corso75/rust-lang

rustc: encode scalar pairs in layout ABI.

src/librustc_trans/abi.rs
@@ -307,6 +307,7 @@ impl<'tcx> LayoutExt<'tcx> for TyLayout<'tcx> {
             layout::Abi::Uninhabited |
             layout::Abi::Scalar(_) |
             layout::Abi::Vector => false,
+            layout::Abi::ScalarPair(..) |
             layout::Abi::Aggregate { .. } => true
         }
     }
@@ -336,6 +337,7 @@ impl<'tcx> LayoutExt<'tcx> for TyLayout<'tcx> {
                 })
             }
 
+            layout::Abi::ScalarPair(..) |
             layout::Abi::Aggregate { .. } => {
                 let mut total = Size::from_bytes(0);
                 let mut result = None;
@@ -741,10 +743,13 @@ impl<'a, 'tcx> FnType<'tcx> {
                         arg.attrs.set(ArgAttribute::NonNull);
                     }
                 }
-                _ => {}
+                _ => {
+                    // Nothing to do for non-pointer types.
+                    return;
+                }
             }
 
-            if let Some(pointee) = arg.layout.llvm_pointee(ccx) {
+            if let Some(pointee) = arg.layout.llvm_pointee(ccx, Size::from_bytes(0)) {
                 if let Some(kind) = pointee.safe {
                     arg.attrs.pointee_size = pointee.size;
                     arg.attrs.pointee_align = Some(pointee.align);

src/librustc_trans/context.rs
@@ -31,7 +31,7 @@ use rustc::middle::trans::Stats;
 use rustc_data_structures::stable_hasher::StableHashingContextProvider;
 use rustc::session::config::{self, NoDebugInfo};
 use rustc::session::Session;
-use rustc::ty::layout::{LayoutError, LayoutOf, TyLayout};
+use rustc::ty::layout::{LayoutError, LayoutOf, Size, TyLayout};
 use rustc::ty::{self, Ty, TyCtxt};
 use rustc::util::nodemap::FxHashMap;
 
@@ -101,7 +101,7 @@ pub struct LocalCrateContext<'a, 'tcx: 'a> {
 
     lltypes: RefCell<FxHashMap<(Ty<'tcx>, Option<usize>), Type>>,
     scalar_lltypes: RefCell<FxHashMap<Ty<'tcx>, Type>>,
-    llpointees: RefCell<FxHashMap<Ty<'tcx>, Option<LlvmPointee>>>,
+    llpointees: RefCell<FxHashMap<(Ty<'tcx>, Size), Option<LlvmPointee>>>,
     isize_ty: Type,
 
     dbg_cx: Option<debuginfo::CrateDebugContext<'tcx>>,
@@ -491,7 +491,8 @@ impl<'b, 'tcx> CrateContext<'b, 'tcx> {
         &self.local().scalar_lltypes
     }
 
-    pub fn llpointees<'a>(&'a self) -> &'a RefCell<FxHashMap<Ty<'tcx>, Option<LlvmPointee>>> {
+    pub fn llpointees<'a>(&'a self)
+                          -> &'a RefCell<FxHashMap<(Ty<'tcx>, Size), Option<LlvmPointee>>> {
         &self.local().llpointees
     }
 

udoprog/rust

src/librustc_trans/abi.rs
@@ -311,6 +311,7 @@ impl<'tcx> LayoutExt<'tcx> for TyLayout<'tcx> {
             layout::Abi::Uninhabited |
             layout::Abi::Scalar(_) |
             layout::Abi::Vector => false,
+            layout::Abi::ScalarPair(..) |
             layout::Abi::Aggregate { .. } => true
         }
     }
@@ -340,6 +341,7 @@ impl<'tcx> LayoutExt<'tcx> for TyLayout<'tcx> {
                 })
             }
 
+            layout::Abi::ScalarPair(..) |
             layout::Abi::Aggregate { .. } => {
                 let mut total = Size::from_bytes(0);
                 let mut result = None;
@@ -745,10 +747,13 @@ impl<'a, 'tcx> FnType<'tcx> {
                         arg.attrs.set(ArgAttribute::NonNull);
                     }
                 }
-                _ => {}
+                _ => {
+                    // Nothing to do for non-pointer types.
+                    return;
+                }
             }
 
-            if let Some(pointee) = arg.layout.pointee_info(ccx) {
+            if let Some(pointee) = arg.layout.pointee_info_at(ccx, Size::from_bytes(0)) {
                 if let Some(kind) = pointee.safe {
                     arg.attrs.pointee_size = pointee.size;
                     arg.attrs.pointee_align = Some(pointee.align);

src/librustc_trans/context.rs
@@ -31,7 +31,7 @@ use rustc::middle::trans::Stats;
 use rustc_data_structures::stable_hasher::StableHashingContextProvider;
 use rustc::session::config::{self, NoDebugInfo};
 use rustc::session::Session;
-use rustc::ty::layout::{LayoutError, LayoutOf, TyLayout};
+use rustc::ty::layout::{LayoutError, LayoutOf, Size, TyLayout};
 use rustc::ty::{self, Ty, TyCtxt};
 use rustc::util::nodemap::FxHashMap;
 use rustc_trans_utils;
@@ -103,7 +103,7 @@ pub struct LocalCrateContext<'a, 'tcx: 'a> {
 
     lltypes: RefCell<FxHashMap<(Ty<'tcx>, Option<usize>), Type>>,
     scalar_lltypes: RefCell<FxHashMap<Ty<'tcx>, Type>>,
-    pointee_infos: RefCell<FxHashMap<Ty<'tcx>, Option<PointeeInfo>>>,
+    pointee_infos: RefCell<FxHashMap<(Ty<'tcx>, Size), Option<PointeeInfo>>>,
     isize_ty: Type,
 
     dbg_cx: Option<debuginfo::CrateDebugContext<'tcx>>,
@@ -516,7 +516,8 @@ impl<'b, 'tcx> CrateContext<'b, 'tcx> {
         &self.local().scalar_lltypes
     }
 
-    pub fn pointee_infos<'a>(&'a self) -> &'a RefCell<FxHashMap<Ty<'tcx>, Option<PointeeInfo>>> {
+    pub fn pointee_infos<'a>(&'a self)
+                             -> &'a RefCell<FxHashMap<(Ty<'tcx>, Size), Option<PointeeInfo>>> {
         &self.local().pointee_infos
     }
 